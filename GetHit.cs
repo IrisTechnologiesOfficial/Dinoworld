29a819db3d51107d9773da0e3a9a8f948e6b0d3d
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-04-22 16:44:06 +0200

    Add files via upload

---
+++ GetHit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GetHit : MonoBehaviour
+{
+    private const string Tag = "Enemy";
+    public Animation hit;
+    public bool Impulse = true;
+    
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.gameObject.tag == Tag)
+        {
+            if(hit != null)
+                hit.Play();
+            if (Impulse)
+            {
+                if(Move.Left)
+                    col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1500f * Time.deltaTime, 0f), ForceMode2D.Impulse);
+                else
+                    col.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1500f * Time.deltaTime, 0f), ForceMode2D.Impulse);
+            }
+        }
+    }
+}
---
+++ Move.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private GameObject player;
+    public static bool Left = false;
+
+    void Start()
+    {
+        if (player == null)
+            player = GameObject.FindWithTag("Player");
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            player.transform.position = new Vector3(player.transform.position.x - (10f * Time.deltaTime), player.transform.position.y, player.transform.position.z);
+            Left = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            player.transform.position = new Vector3(player.transform.position.x + (10f * Time.deltaTime), player.transform.position.y, player.transform.position.z);
+            Left = false;
+        }
+    }
+}
