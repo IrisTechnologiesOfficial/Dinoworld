9187abee6c74125779423d342ee45743b2365869
IrisTechnologiesOfficial <40865154+IrisTechnologiesOfficial@users.noreply.github.com>
2019-04-24 02:28:10 +0200

    Add files via upload

---
+++ ClickActivateDeactivate.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ClickActivateDeactivate : MonoBehaviour
+{
+    public struct ActivateDeactivateElement
+    {
+        public RectTransform ElementWhichWillActivateOrDeactivate;
+
+        public GameObject[] ObjectsToActivateOrDeactivate;
+
+        public bool ActivateOrDeactivate;
+    }
+
+    private bool MousePressed = false;
+    public ActivateDeactivateElement[] Elements;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            MousePressed = true;
+        if (Input.GetMouseButtonUp(0))
+            MousePressed = false;
+        if (MousePressed)
+            foreach (ActivateDeactivateElement element in Elements)
+            {
+                Vector2 localMousePosition = element.ElementWhichWillActivateOrDeactivate.InverseTransformPoint(Input.mousePosition);
+                if (element.ElementWhichWillActivateOrDeactivate.rect.Contains(localMousePosition))
+                {
+                    if (element.ActivateOrDeactivate)
+                        foreach (GameObject go in element.ObjectsToActivateOrDeactivate)
+                            go.SetActive(true);
+                    else
+                        foreach (GameObject go in element.ObjectsToActivateOrDeactivate)
+                            go.SetActive(false);
+                }
+            }
+    }
+}
